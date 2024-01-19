78977175bf77d2988ca808f23de7d85486e7d30e
RByunsu <yss@rightbrain.co.kr>
2024-01-19 15:59:30 +0900

    수정 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
+import screenImg from './img/Browser_Screen_Ratio.jpg';
 
 function App() {
 
   return (
     <>
-      <img src='./Browser_Screen_Ratio.jpg' />
+      <img src={screenImg} alt='' />
     </>
   )
 }
